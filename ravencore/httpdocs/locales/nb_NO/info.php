f7cff1bce73b708478538b0aaf541f877a94ee3a
Corey Henderson <corman@cormander.com>
2006-01-05 19:20:36 -0700

    Re-work language directory structure
    Each language pack now has two files, one for metadata "info",
    and the data for all the actual translations.
    
    Also adds locale functions that are now used everywhere.

---
+++ ravencore/httpdocs/locales/nb_NO/info.php
@@ -0,0 +1,13 @@
+<?php
+
+$locales['nb_NO'] = array(
+			  'name' => 'Norwegian',
+			  'charset' => 'iso-8859-15',
+			  'date_format' => '',
+			  'filemanager' => 'norwegian',
+			  'phpmyadmin' => 'no-utf-8',
+			  'sysinfo' => 'no',
+			  'awstats' => 'nb'
+			  );
+
+?>
