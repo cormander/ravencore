40e945c936d69a79ebc6c7ce472fbf5f34eb58a7
Corey Henderson <corman@cormander.com>
2006-01-11 18:48:35 -0700

    Adding Russian language support

---
+++ ravencore/httpdocs/locales/ru_RU/info.php
@@ -0,0 +1,13 @@
+<?php
+
+$locales['ru_RU'] = array(
+			  'name' => 'Russian',
+			  'charset' => 'windows-1251',
+			  'date_format' => '',
+			  'filemanager' => 'russian',
+			  'phpmyadmin' => 'ru-utf-8',
+			  'sysinfo' => 'ru',
+			  'awstats' => 'ru'
+			  );
+
+?>
