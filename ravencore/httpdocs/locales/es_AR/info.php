d676b808f6f34ab76b076ab540dd01a8d5c22eda
Corey Henderson <corman@cormander.com>
2006-02-22 17:07:50 -0700

    Add spanish language support

---
+++ ravencore/httpdocs/locales/es_AR/info.php
@@ -0,0 +1,16 @@
+<?php
+
+$locales['es_AR'] = array(
+			  // name is what appears on the login page language selection
+			   'name' => 'Spanish',
+			   // charset shows up in the HTML head section
+			   'charset' => 'iso-8859-1',
+			   'date_format' => '',
+			   // the below are so we know how to pass this language to our 3rd party programs
+			   'filemanager' => 'spanish',
+			   'phpmyadmin' => 'es-utf-8',
+			   'sysinfo' => 'es',
+			   'awstats' => 'es'
+			   );
+
+?>
