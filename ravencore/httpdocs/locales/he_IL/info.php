6d980b8ebb00516030be5f218f2eab015bf19883
Corey Henderson <corman@cormander.com>
2006-02-22 17:52:52 -0700

    Add Hebrew language support

---
+++ ravencore/httpdocs/locales/he_IL/info.php
@@ -0,0 +1,16 @@
+<?php
+
+$locales['he_IL'] = array(
+			  // name is what appears on the login page language selection
+			   'name' => 'Hebrew',
+			   // charset shows up in the HTML head section
+			   'charset' => 'windows-1255',
+			   'date_format' => '',
+			   // the below are so we know how to pass this language to our 3rd party programs
+			   'filemanager' => 'hebrew',
+			   'phpmyadmin' => 'he-utf-8',
+			   'sysinfo' => 'he',
+			   'awstats' => 'he'
+			   );
+
+?>
