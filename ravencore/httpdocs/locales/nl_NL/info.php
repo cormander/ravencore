2233cfb08a678e9ebe8f4a40e8b7dbe91b92e968
Corey Henderson <corman@cormander.com>
2006-02-23 17:42:34 -0700

    Add Dutch language support

---
+++ ravencore/httpdocs/locales/nl_NL/info.php
@@ -0,0 +1,16 @@
+<?php
+
+$locales['nl_NL'] = array(
+			  // name is what appears on the login page language selection
+			   'name' => 'Dutch',
+			   // charset shows up in the HTML head section
+			   'charset' => 'iso-8859-1',
+			   'date_format' => '',
+			   // the below are so we know how to pass this language to our 3rd party programs
+			   'filemanager' => 'dutch',
+			   'phpmyadmin' => 'du-utf-8',
+			   'sysinfo' => 'dutch',
+			   'awstats' => 'du'
+			   );
+
+?>
