0b18ecb015d2a2a30c7e04391354abf71e4d5da4
Corey Henderson <corman@cormander.com>
2009-03-14 15:57:59 -0600

    Add Turkish language support

---
+++ ravencore/httpdocs/locales/tr_TR/info.php
@@ -0,0 +1,16 @@
+<?php
+
+$locales['tr_TR'] = array(
+			  // name is what appears on the login page language selection
+			   'name' => 'Turkish',
+			   // charset shows up in the HTML head section
+			   'charset' => 'iso-8859-9',
+			   'date_format' => 'd/m/Y',
+			   // the below are so we know how to pass this language to our 3rd party programs
+			   'filemanager' => 'turkce',
+			   'phpmyadmin' => 'iso8859-9',
+			   'sysinfo' => 'tr',
+			   'awstats' => 'tr'
+			   );
+
+?>
