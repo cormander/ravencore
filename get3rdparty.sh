47c86d40d096604c767a9f6cd9150b8554950959
Corey Henderson <corman@cormander.com>
2009-04-07 17:08:58 -0600

    Be a little more specific about what get3rdparty.sh does

--- get3rdparty.sh
+++ get3rdparty.sh
@@ -20,6 +20,8 @@ if [ $? -ne 0 ]; then
 		exit 1
 	fi
 
+	# search local RPM sources first, then the given vendor URL, then the ravencore
+	# download site. As soon as a valid download is found, exit
 	for i in "file://$(rpm --eval '%{_sourcedir}')/$PROG" "$URL" "http://download.ravencore.com/ravencore/3rdparty/$PROG"; do
 		echo "Trying to fetch from $i"
 		curl -L $i -o src/$PROG
@@ -32,7 +34,7 @@ if [ $? -ne 0 ]; then
 		fi
 	done
 
-	echo "No more URLs to try"
+	echo "No more URLs to try for $PROG"
 	exit 1
 
 fi
