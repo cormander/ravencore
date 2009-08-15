b223faef47e21d31262f178923f57bc750bcb77a
Corey Henderson <corman@cormander.com>
2009-08-15 16:49:49 -0600

    Increase size of login for login_failure table
    Since emails can be used as usernames now, they get truncated
    by the limited length of this field, making brute force attempts
    for the password of an email address possible via the control
    panel. This should fix that.

--- ravencore/etc/sql_tables/login_failure.sql
+++ ravencore/etc/sql_tables/login_failure.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS login_failure;
 CREATE TABLE login_failure (
   id int(10) unsigned NOT NULL auto_increment,
-  login varchar(15) NOT NULL default '',
+  login varchar(255) NOT NULL default '',
   date datetime NOT NULL default '0000-00-00 00:00:00',
   PRIMARY KEY  (id)
 ) TYPE=MyISAM;
