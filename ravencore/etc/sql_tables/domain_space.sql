028b9f8d0da5e1400dda409b671412a5f14b347e
Corey Henderson <corman@cormander.com>
2009-08-29 14:30:03 -0600

    Add missing date column to sqlite tables

--- ravencore/etc/sql_tables/domain_space.sql
+++ ravencore/etc/sql_tables/domain_space.sql
@@ -2,6 +2,7 @@ CREATE TABLE domain_space (
   id integer not null primary key autoincrement unique,
   did int(10),
   type varchar(10),
+  date int,
   bytes int(10),
   varchar datetime(10)
 );
--- ravencore/etc/sql_tables/domain_traffic.sql
+++ ravencore/etc/sql_tables/domain_traffic.sql
@@ -2,6 +2,7 @@ CREATE TABLE domain_traffic (
   id integer not null primary key autoincrement unique,
   did int(10),
   type varchar(10),
+  date int,
   bytes int(10),
   datetime varchar(10)
 );
